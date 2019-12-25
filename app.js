1aa3f2442072cb0417c22de73412b60b1b08d5a9
Niqi <bkbeauty416@gmail.com>
2019-12-24 19:02:25 -0500

    Added files

---
+++ app.js
@@ -0,0 +1,47 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var favicon = require('serve-favicon');	
+var stylus = require('stylus');
+var helmet = require('helmet');
+
+var app = express();
+
+var router = require('./routes/routes');  //Router
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router);
+
+//Favicon - uncomment when ready to use
+//app.use(favicon(path.join(__dirname, 'public', 'cleanBucket.png')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(4016);
+console.log('Server started on port 4016');
+module.exports = app;
